b714fabc7fb550e80df93a2d2167419c985a6b05
unknown <Juan Cruz>
2023-05-23 20:56:18 -0300

    adding spring files

---
+++ src/main/java/com/unla/jcvspring/JcvSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unla.jcvspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcvSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcvSpringApplication.class, args);
+	}
+
+}
